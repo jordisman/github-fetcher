eeb927fb9ed641d0d1b8d066b9a903be38eaf33e
Jordis Man <jordisman@gmail.com>
2019-12-07 15:23:30 -0800

    Create helper file for getRepoByUser function

---
+++ server/helpers.js
@@ -0,0 +1,15 @@
+const config = require('./config.js');
+
+module.exports.getRepoByUser = (username, cb) => {
+  let options = {
+    url: `https://api.github.com/users/${username}/repos`,
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': `token ${config.TOKEN}`
+    }
+  };
+
+  request(options, (err, res, body) => {
+    cb(JSON.parse(body));
+  });
+}
