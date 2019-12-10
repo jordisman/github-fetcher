bc5ac9a9267401a133c5991420a527da22dbabfc
Jordis Man <jordisman@gmail.com>
2019-12-09 16:05:51 -0800

    Added data.json

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const db = require('../database/index.js');
+const helpers = require('./helpers.js');
 const bodyParser = require('body-parser');
 const express = require('express');
 const morgan = require('morgan');
@@ -13,13 +14,14 @@ app.use(express.static(__dirname + '/../client/dist'));
 
 app.post('/repos', function (req, res) {
   let saveRepos = repos => repos.forEach(db.save);
-  github.getReposByUsername(req.body, saveRepos);
+  helpers.getReposByUsername(req.body.username, saveRepos);
   res.status(200).send('repos posted!');
 });
 
 app.get('/repos', function (req, res) {
-  res.status(200);
-  db.fetch(res.send.bind(res));
+  db.find(repos => {
+    res.status(200).send(repos);
+  });
 });
 
 
